d0eb3e609105abf85147cf18d4153f0d5e770973
Ivan Popal <maincorp228@gmail.com>
2016-03-29 13:55:28 +0400

    Мелкая правка #3

--- build/js/check.js
+++ build/js/check.js
@@ -1,37 +1,31 @@
 function getMessage (a, b) {
-	var typeA  = typeof(a);
-	var typeB  = typeof(B);
+    var typeA = typeof(a);
+    var typeB = typeof(b);
 
-	if (typeA === "boolean") {
-		if (a == true) {
-			return "Я попал в " + [b];
-		} else {
-			return "Я никуда не попал";
-		}
-	}
+    if (typeA == "boolean") {
+        if (a == true) {
+            return "Я попал в " + b;
+        } else {
+            return "Я никуда не попал";
+        }
+    }
 
-	if (typeA === "number") {
-		return "Я прыгнул на " + [a] * 100 + " сантиметров";
-	} 
+    if (typeA == "number") {
+        return "Я прыгнул на " + a * 100 + " сантиметров";
+    }
 
-	if (typeA === "object") {
-		var sum = 0;
-		for (var i = 0; i < a.length; i++) {
-			sum += a[i];
-		} 
-		return  "Я прошёл " + [sum] + " шагов";
-	} else {
-		return  "Я прошёл нисколько шагов";
-	}
-
-	if (typeA === "object" && typeB === "object") {
-		var length = 0;
-		for (var i = 0; i < a.length && i < b.length; i++) {
-			length += a[i] * b[i];
-		}
-		return "Я прошёл" + [length] + " метров";
-	} else {
-		return "Я не прошёл и одного метра";
-	}
+    if ((Array.isArray(a) == true) && (Array.isArray(b) == false)) {
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+            sum += a[i];
+        } 
+        return  "Я прошёл " + sum + " шагов";
+    } else if ((Array.isArray(a) == true) && (Array.isArray(b) == true)) {
+        var length = 0;
+        for (var i = 0; i < a.length && i < b.length; i++) {
+            length += a[i] * b[i];
+        }
+        return "Я прошёл " + length + " метров";
+    }
 
 }
