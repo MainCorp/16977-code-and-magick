74bc146040d03e5c8d485d5789a6af737a300d4e
Ivan Popal <maincorp228@gmail.com>
2016-03-31 20:45:12 +0400

    Валидация

--- src/form.js
+++ src/form.js
@@ -4,6 +4,12 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formUserName = document.querySelector('#review-name');
+  var formFieldsName = document.querySelector('.review-fields-name');
+  var formUserText = document.querySelector('#review-text');
+  var formFieldsText = document.querySelector('.review-fields-text');
+  var formReviewMark_1 = document.querySelector('#review-mark-1');
+  var formReviewMark_2 = document.querySelector('#review-mark-2');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -13,5 +19,21 @@
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.add('invisible');
+  };
+
+  formUserName.oninput = function(event) {
+    event.preventDefault();
+    formFieldsName.classList.add('invisible');
+  };
+  formUserText.oninput = function(event) {
+    event.preventDefault();
+    formFieldsText.classList.add('invisible');
+  };
+
+  formReviewMark_1.onclick = function() {
+    formUserText.setAttribute('required', '');
+  };
+  formReviewMark_2.onclick = function() {
+    formUserText.setAttribute('required', '');
   };
 })();
