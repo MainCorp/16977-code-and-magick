b7bb1bc18606696119ff3263b4e877e4ae8f7c2a
Ivan Popal <maincorp228@gmail.com>
2016-04-02 23:36:43 +0400

    Мелкая правка #4

--- build/js/check.js
+++ build/js/check.js
@@ -2,30 +2,34 @@ function getMessage (a, b) {
     var typeA = typeof(a);
     var typeB = typeof(b);
 
-    if (typeA == "boolean") {
-        if (a == true) {
+    if (typeA === "boolean") {
+        if (a) {
             return "Я попал в " + b;
         } else {
             return "Я никуда не попал";
         }
     }
 
-    if (typeA == "number") {
+    if (typeA === "number") {
         return "Я прыгнул на " + a * 100 + " сантиметров";
     }
 
-    if ((Array.isArray(a) == true) && (Array.isArray(b) == false)) {
-        var sum = 0;
-        for (var i = 0; i < a.length; i++) {
-            sum += a[i];
-        } 
-        return  "Я прошёл " + sum + " шагов";
-    } else if ((Array.isArray(a) == true) && (Array.isArray(b) == true)) {
-        var length = 0;
+    if (!(Array.isArray(a))) {
+        return "Ошибка";
+    } 
+    if ((Array.isArray(b) == true)) {
+    	var length = 0;
         for (var i = 0; i < a.length && i < b.length; i++) {
             length += a[i] * b[i];
         }
         return "Я прошёл " + length + " метров";
+    }
+    if ((Array.isArray(a) == true)) {
+    	var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+            sum += a[i];
+        } 
+        return  "Я прошёл " + sum + " шагов";
     }
 
 }
