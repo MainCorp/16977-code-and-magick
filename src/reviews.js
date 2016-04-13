8557dab6511d1693cea5b6934e1913d517e87460
Ivan Popal <maincorp228@gmail.com>
2016-04-13 16:05:13 +0400

    Шаблонизация, правка #1

---
+++ src/reviews.js
@@ -0,0 +1,33 @@
+var reviewsContainer = document.querySelector('.reviews-list');
+var templateEl = document.querySelector('template');
+var elementClone;
+
+if ('content' in templateEl) {
+	elementClone = templateEl.content.querySelector('.review');
+} else {
+	elementClone = templateEl.querySelector('.review');
+}
+
+var getReviewElement = function(data, container) {
+	var element = elementClone.cloneNode(true);
+	element.querySelector('.review-text').textContent = data.description;
+	container.appendChild(element);
+
+	var authorPicture = new Image();
+
+	authorPicture.onload = function(evt) {
+		element.style.authorPicture = 'url(\'' + evt.target.src + '\')';
+	};
+
+	authorPicture.onerror = function() {
+		element.classList.add('review-load-failure');
+	};
+
+	authorPicture.src = '/' + data.author.picture;
+
+	return element;
+};
+
+window.reviews.forEach(function(review) {
+	getReviewElement(review, reviewsContainer);
+});
