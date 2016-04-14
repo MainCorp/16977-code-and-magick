7dcc14e7ee547169d41a2fccf8b9985542beb198
Ivan Popal <maincorp228@gmail.com>
2016-04-14 18:53:23 +0400

    Робкое знакомство с AJAX . Правка #1

--- src/reviews.js
+++ src/reviews.js
@@ -1,6 +1,11 @@
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateEl = document.querySelector('template');
 var elementClone;
+var reviewsContent = document.querySelector('.reviews');
+
+/** @const {number} */
+
+var REVIEWS_URL = '//o0.github.io/assets/json/reviews.json';
 
 if ('content' in templateEl) {
 	elementClone = templateEl.content.querySelector('.review');
@@ -28,6 +33,39 @@ var getReviewElement = function(data, container) {
 	return element;
 };
 
-window.reviews.forEach(function(review) {
-	getReviewElement(review, reviewsContainer);
+var getReviews = function(callback) {
+	var xhr = new XMLHttpRequest();
+
+	xhr.onload = function(evt) {
+		var requestObj = evt.target;
+		var loadedData = JSON.parse(requestObj.response);
+		callback(loadedData);
+	};
+	xhr.onerror = function() {
+		reviewsContent.classList.add('reviews-load-failure');
+	};
+	xhr.timeout = 15000;
+	xhr.ontimeout = function() {
+		reviewsContent.classList.add('reviews-load-failure');
+	};
+
+	xhr.open('GET', REVIEWS_URL);
+	xhr.onreadystatechange = function() {
+		if(xhr.status != 200) {
+			reviewsContent.classList.add('reviews-list-loading');
+		}
+	};
+	xhr.send();
+};
+
+/** @param {Array.<Object>} reviews */
+var renderReviews = function(reviews) {
+	reviews.forEach(function(review) {
+		getReviewElement(review, reviewsContainer);
+	});
+};
+
+getReviews(function(loadedReviews) {
+	reviews = loadedReviews;
+	renderReviews(reviews);
 });
