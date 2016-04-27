16bc33d48fe55ee1909fe188acb30f73897e8e1f
Ivan Popal <maincorp228@gmail.com>
2016-04-28 03:46:13 +0400

    Робкое знакомство с AJAX . filter, правка #4

--- src/reviews.js
+++ src/reviews.js
@@ -5,117 +5,109 @@ var elementToClone = templateElement.content.querySelector('.review');
 
 var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
 
-
 var getReviewsElement = function(data, container) {
-	var element = elementToClone.cloneNode(true);
-	element.querySelector('.review-text').textContent = data.description;
-	container.appendChild(element);
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.review-text').textContent =	data.description;
+  container.appendChild(element);
 
-	var photoUser = new Image();
-	var reviewAuthor = element.querySelector('.review-author');
+  var photoUser = new Image();
+  var reviewAuthor =	element.querySelector('.review-author');
 
-	photoUser.onload = function(evt) {
-		reviewAuthor.style.backgroundImage = 'url(\'' + evt.target.src + '\')';
-		reviewAuthor.style.height = '124px';
-		reviewAuthor.style.width = '124px';
-	};
+  photoUser.onload = function(evt) {
+    reviewAuthor.style.backgroundImage = 'url(\'' + evt.target.src + '\')';
+    reviewAuthor.style.height = '124px';
+    reviewAuthor.style.width = '124px';
+  };
 
-	photoUser.onerror = function() {
-		element.classList.add('review-load-failure');
-	};
+  photoUser.onerror = function() {
+    element.classList.add('review-load-failure');
+  };
 
-	photoUser.src = data.author.picture;
+  photoUser.src = data.author.picture;
 
-	return element;
+  return element;
 };
 
 var getReviews = function(callback) {
-	var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-	xhr.onload = function(evt) {
-		try {
-			var loadedData = JSON.parse(evt.target.response);
-		} catch (err) {
-				console.log('Ошибка соединения с сервером');
-		}
+  xhr.onload = function(evt) {
+    try {
+      var loadedData = JSON.parse(evt.target.response);
+    } catch (err) {
+      console.log('Ошибка соединения с сервером');
+    }
 
-		callback(loadedData);
-	};
+    callback(loadedData);
+  };
 
-	xhr.onerror = function() {
-		elementToClone.classList.add('reviews-load-failure');
-	};
-	xhr.timeout = 15000;
-	xhr.ontimeout = function() {
-		elementToClone.classList.add('reviews-load-failure');
-	};
+  xhr.onerror = function() {
+    elementToClone.classList.add('reviews-load-failure');
+  };
+  xhr.timeout = 15000;
+  xhr.ontimeout = function() {
+    elementToClone.classList.add('reviews-load-failure');
+  };
 
-	xhr.open('GET', REVIEWS_LOAD_URL);
-
-	xhr.onreadystatechange = function () {
-		if (xhr.status !== 200) {
-			elementToClone.classList.add('reviews-list-loading');
-		}
-	};
-
-	xhr.send();
+  xhr.open('GET', REVIEWS_LOAD_URL);
+  xhr.send();
 };
 
 var renderReviews = function(reviews) {
-	reviewsContainer.innerHTML = '';
+  reviewsContainer.innerHTML = '';
 
-	reviews.forEach(function(review) {
-		getReviewsElement(review, reviewsContainer);
-	});
+  reviews.forEach(function(review) {
+    getReviewsElement(review, reviewsContainer);
+  });
 };
 
 var getFilteredReviews = function(reviews, filter) {
-	var reviewsToFilter = reviews.slice(0);
+  var reviewsToFilter = reviews.slice(0);
 
-	switch (filter) {
-		case 'reviews-all':
-			break;
-		case 'reviews-recent':
-			reviewsToFilter.sort(function(a, b) {
-				return a.data - b.data;
-			});
-			break;
-		case 'reviews-good':
-			reviewsToFilter.sort(function(a, b) {
-				return a.rating - b.rating;
-			});
-			break;
-		case 'reviews-bad':
-			reviewsToFilter.sort(function(a, b) {
-				return a.rating - b.rating;
-			});
-			break;
-		case 'reviews-popular':
-			reviewsToFilter.sort(function(a, b) {
-				return a.review_usefulness - b.review_usefulness;
-			});
-			break;
-	}
+  switch (filter) {
+    case 'reviews-all':
+      break;
+    case 'reviews-recent':
+      reviewsToFilter.sort(function(a, b) {
+        return (new Date(b.date) - new Date(a.date));
+      });
+      break;
+    case 'reviews-good':
+      reviewsToFilter.sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+      break;
+    case 'reviews-bad':
+      reviewsToFilter.sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+      break;
+    case 'reviews-popular':
+      reviewsToFilter.sort(function(a, b) {
+        return a.review_usefulness - b.review_usefulness;
+      });
+      break;
+  }
 
-	return reviewsToFilter;
+  return reviewsToFilter;
 };
 
 var includeFilter = function(filter) {
-	var filteredReviews = getFilteredReviews(reviews, filter);
-		renderReviews(filteredReviews);
+  var filteredReviews = getFilteredReviews(reviews, filter);
+  renderReviews(filteredReviews);
 };
 
 var includeFiltration = function() {
-	var filters = filtersContainer.querySelectorAll('.reviews-filter-item');
-	for (var i = 0; i < filters.length; i++) {
-		filters[i].onclick = function(evt) {
-			includeFilter(this.id);
-		};
-	}
+  var filters = filtersContainer.querySelectorAll('.reviews-filter-item');
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function() {
+      includeFilter(this.id);
+    };
+  }
 };
 
 getReviews(function(loadedReviews) {
-	reviews = loadedReviews;
-	includeFiltration(true);
-	includeFilter('reviews-all');
+  reviews = loadedReviews;
+  includeFiltration(true);
+  includeFilter('reviews-all');
 });
