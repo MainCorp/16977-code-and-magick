08734ba161cf2d64688c6aea6f1c6ff8fc176491
Ivan Popal <maincorp228@gmail.com>
2016-05-03 01:13:24 +0400

    Робкое знакомство с AJAX . Внесение правок

--- src/form.js
+++ src/form.js
@@ -1,16 +1,22 @@
 'use strict';
 
 (function() {
+  var cookies = require('browser-cookies');
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
-  var formUserName = document.querySelector('#review-name');
-  var formFieldsName = document.querySelector('.review-fields-name');
-  var formUserText = document.querySelector('#review-text');
-  var formFieldsText = document.querySelector('.review-fields-text');
-  var formReviewMark_1 = document.querySelector('#review-mark-1');
-  var formReviewMark_2 = document.querySelector('#review-mark-2');
-  var formButton = document.querySelector('.review-submit');
+
+  var reviewName = document.querySelector('#review-name');
+  var reviewText = document.querySelector('#review-text');
+  var reviewRating = document.querySelectorAll('input[name="review-mark"]');
+  var toolReviewName = document.querySelector('.review-fields-name');
+  var toolReviewText = document.querySelector('.review-fields-text');
+  var reviewSubmit = document.querySelector('.review-submit');
+  var reviewFields = document.querySelector('.review-fields');
+  var reviewForm = document.querySelector('.review-form');
+  var rating = reviewForm['review-mark'];
+  reviewSubmit.disabled = true;
+  reviewName.required = true;
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -22,37 +28,63 @@
     formContainer.classList.add('invisible');
   };
 
-  formUserName.oninput = function(evt) {
+  var validationSendingResponse = function() {
+    var validationToolName = reviewName.value.length > 0;
+    var validationToolText = reviewText.value.length > 0;
+    var validationRating = rating.value > 2;
+
+    toolReviewName.classList.toggle('invisible', validationToolName);
+    toolReviewText.classList.toggle('invisible', validationRating || validationToolText);
+    reviewFields.classList.toggle('invisible', validationToolName && (validationToolText || validationRating));
+    if (validationToolName && (validationRating || validationToolText)) {
+      reviewSubmit.disabled = false;
+    } else if (!validationToolText || !validationToolName) {
+      reviewSubmit.disabled = true;
+    }
+  };
+
+  for (var i = 0; i < reviewRating.length; i++) {
+    reviewRating[i].onclick = function() {
+      reviewText.required = this.value < 3;
+      validationSendingResponse();
+    };
+  }
+
+  formOpenButton.onclick = function(evt) {
     evt.preventDefault();
-    formFieldsName.classList.add('invisible');
+    formContainer.classList.remove('invisible');
+    validationSendingResponse();
   };
 
-  formUserName.oninvalid = function(evt) {
+  reviewName.oninput = validationSendingResponse;
+
+  reviewText.oninput = validationSendingResponse;
+
+  /* Срок жизни куки */
+
+  var cookiesLifeTerm = function() {
+    var today = new Date();
+    var lastBirthday = new Date(today.getFullYear(), 6, 4);
+    var cookiesLife = Math.floor((today - lastBirthday) / 24 / 60 / 60 / 1000);
+
+    if (lastBirthday > today) {
+      lastBirthday.setFullYear(lastBirthday.getFullYear() - 1);
+      cookiesLife = Math.floor((today - lastBirthday) / 24 / 60 / 60 / 1000);
+    }
+
+    return cookiesLife;
+  };
+
+
+  reviewForm.onsubmit = function(evt) {
     evt.preventDefault();
-    formButton.setAttribute('disabled', '');
+    cookies.set('Username', reviewName.value, {expires: cookiesLifeTerm()});
+    cookies.set('Assessment', rating.value, {expires: cookiesLifeTerm()});
+
+    this.submit();
   };
 
-  formUserText.oninput = function(evt) {
-    evt.preventDefault();
-    formFieldsText.classList.add('invisible');
-  };
-
-  formReviewMark_1.onclick = function(evt) {
-    formUserText.setAttribute('required', '');
-  };
-
-  formReviewMark_2.onclick = function(evt) {
-    formUserText.setAttribute('required', '');
-  };
-
-  formButton.onsubmit = function(evt) {
-    evt.preventDefault();
-    var dateToExpire = Date.now + 26092800000;
-    document.cookie = 'name=' + 'test';
-
-    formButton.submit();
-  };
-
+  reviewName.value = cookies.get('Username');
+  rating.value = cookies.get('Assessment');
 
 })();
-
--- src/reviews.js
+++ src/reviews.js
@@ -1,17 +1,26 @@
-var filtersContainer = document.querySelector('.reviews-filter');
+'use strict';
+
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone = templateElement.content.querySelector('.review');
 
-var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
 
-var getReviewsElement = function(data, container) {
+var receiveReviewsElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
-  element.querySelector('.review-text').textContent =	data.description;
+  element.querySelector('.review-text').textContent = data.description;
+  var reviewRating = element.querySelector('.review-rating');
+  var reviewRatingClass = ['review-rating-two', 'review-rating-three', 'review-rating-four', 'review-rating-five'];
+
+  var calcRating = function(ratingNumber) {
+    reviewRating.classList.add(reviewRatingClass[ratingNumber]);
+  };
+  calcRating(data.rating - 2);
+
   container.appendChild(element);
 
   var photoUser = new Image();
-  var reviewAuthor =	element.querySelector('.review-author');
+  var reviewAuthor = element.querySelector('.review-author');
+  reviewAuthor.src = data.author.picture;
 
   photoUser.onload = function(evt) {
     reviewAuthor.style.backgroundImage = 'url(\'' + evt.target.src + '\')';
@@ -23,91 +32,10 @@ var getReviewsElement = function(data, container) {
     element.classList.add('review-load-failure');
   };
 
-  photoUser.src = data.author.picture;
 
   return element;
 };
 
-var getReviews = function(callback) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.onload = function(evt) {
-    try {
-      var loadedData = JSON.parse(evt.target.response);
-    } catch (err) {
-      console.log('Ошибка соединения с сервером');
-    }
-
-    callback(loadedData);
-  };
-
-  xhr.onerror = function() {
-    elementToClone.classList.add('reviews-load-failure');
-  };
-  xhr.timeout = 15000;
-  xhr.ontimeout = function() {
-    elementToClone.classList.add('reviews-load-failure');
-  };
-
-  xhr.open('GET', REVIEWS_LOAD_URL);
-  xhr.send();
-};
-
-var renderReviews = function(reviews) {
-  reviewsContainer.innerHTML = '';
-
-  reviews.forEach(function(review) {
-    getReviewsElement(review, reviewsContainer);
-  });
-};
-
-var getFilteredReviews = function(reviews, filter) {
-  var reviewsToFilter = reviews.slice(0);
-
-  switch (filter) {
-    case 'reviews-all':
-      break;
-    case 'reviews-recent':
-      reviewsToFilter.sort(function(a, b) {
-        return (new Date(b.date) - new Date(a.date));
-      });
-      break;
-    case 'reviews-good':
-      reviewsToFilter.sort(function(a, b) {
-        return a.rating - b.rating;
-      });
-      break;
-    case 'reviews-bad':
-      reviewsToFilter.sort(function(a, b) {
-        return a.rating - b.rating;
-      });
-      break;
-    case 'reviews-popular':
-      reviewsToFilter.sort(function(a, b) {
-        return a.review_usefulness - b.review_usefulness;
-      });
-      break;
-  }
-
-  return reviewsToFilter;
-};
-
-var includeFilter = function(filter) {
-  var filteredReviews = getFilteredReviews(reviews, filter);
-  renderReviews(filteredReviews);
-};
-
-var includeFiltration = function() {
-  var filters = filtersContainer.querySelectorAll('.reviews-filter-item');
-  for (var i = 0; i < filters.length; i++) {
-    filters[i].onclick = function() {
-      includeFilter(this.id);
-    };
-  }
-};
-
-getReviews(function(loadedReviews) {
-  reviews = loadedReviews;
-  includeFiltration(true);
-  includeFilter('reviews-all');
+window.reviews.forEach(function(review) {
+  receiveReviewsElement(review, reviewsContainer);
 });
