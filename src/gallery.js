aaad7886528f62344f871dcf4f264d43abc8de9e
Ivan Popal <maincorp228@gmail.com>
2016-05-16 09:46:38 +0400

    Первый класс v6

--- src/gallery.js
+++ src/gallery.js
@@ -8,6 +8,7 @@ var previewNumber = galleryContainer.querySelector('.preview-number-current');
 var imgPrev = galleryContainer.querySelector('.overlay-gallery-control-left');
 var imgNext = galleryContainer.querySelector('.overlay-gallery-control-right');
 var btnCloseGallery = galleryContainer.querySelector('.overlay-gallery-close');
+var photoGallery = document.querySelector('.photogallery');
 var photos = [];
 var lengthArrayPhotos = photos.length;
 imgPrev.classList.toggle('invisible', true);
@@ -53,18 +54,24 @@ function _changePhoto() {
 
 function _hideGallery() {
   galleryContainer.classList.add('invisible');
-
-  imgPrev.removeEventListener('click', _showPrevImage);
-  imgNext.removeEventListener('click', _showNextImage);
   btnCloseGallery.removeEventListener('click', _onCloseClick);
 
   window.removeEventListener('keydown', _onDocumentKeyDown);
 }
 
-function getPhotos() {
-  for (var i = 0; i < imgCollection.length; i++) {
-    photos.push(imgCollection[i].getAttribute('src'));
-    imgCollection[i].dataset.id = i;
+function getActivePhoto(param) {
+  for (var i = 0; i < photos.length; i++) {
+    if(photos[i] === param) {
+      break;
+    }
+  }
+  return i;
+}
+
+getPhotos(imgCollection);
+function getPhotos(pct) {
+  for (var i = 0; i < pct.length; i++) {
+    photos[i] = pct[i].src;
   }
   lengthArrayPhotos = photos.length;
 
@@ -72,10 +79,7 @@ function getPhotos() {
   currentPhoto = galleryPreview.appendChild(new Image());
 }
 
-function showGallery(idPhoto) {
-  numberPhoto = idPhoto;
-  currentPhoto.src = photos[numberPhoto];
-  previewNumber.textContent = numberPhoto + 1;
+function showGallery() {
   galleryContainer.classList.remove('invisible');
 
   imgPrev.addEventListener('click', _showPrevImage);
@@ -83,8 +87,16 @@ function showGallery(idPhoto) {
   btnCloseGallery.addEventListener('click', _onCloseClick);
 
   window.addEventListener('keydown', _onDocumentKeyDown);
+
+  _changePhoto();
 }
 
+photoGallery.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  if (evt.target.tagName === 'IMG') {
+    numberPhoto = getActivePhoto(evt.target.src);
+    showGallery();
+  }
+});
 
-module.exports.getPhotos = getPhotos;
 module.exports.showGallery = showGallery;
