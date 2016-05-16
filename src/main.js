63476ce7e73ec4ce25a816e8b708d02f4d9373fc
Ivan Popal <maincorp228@gmail.com>
2016-05-16 09:09:04 +0400

    Объект галлерея, начало v8

--- src/main.js
+++ src/main.js
@@ -1,17 +1,4 @@
 require('./form');
 require('./game');
 require('./reviews-cont/reviews');
-var gallery = require('./gallery');
-
-var photoGallery = document.querySelector('.photogallery');
-var idPhoto;
-gallery.getPhotos();
-
-photoGallery.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (evt.target.tagName === 'IMG') {
-    idPhoto = gallery.returnSerialNumber(photos.src);
-    gallery.showGallery(idPhoto);
-  }
-  console.log(returnSerialNumber(photos.src));
-});
+require('./gallery');
